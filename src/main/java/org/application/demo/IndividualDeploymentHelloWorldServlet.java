d6de6620b50e887c702e29dd4e4b4eef570d746c
Andreas Mayer <andreas.mayer@gepardec.com>
2022-01-06 22:00:52 +0100

    - added individual deployment via web.xml instead of @WebServlet() annotation

---
+++ src/main/java/org/application/demo/IndividualDeploymentHelloWorldServlet.java
@@ -0,0 +1,35 @@
+package org.application.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IndividualDeploymentHelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /*
+    Servlet Lifecycle
+    1.) init()
+    2.) service(ServletRequest, ServletResponse)
+    3.) destroy()
+
+    Implementation:
+    HttpServlet
+     */
+
+    public IndividualDeploymentHelloWorldServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("Served at: ").append(req.getContextPath()).append("\n Example of individual servlet deployment setup over web.xml. Not needed if annotation @WebServlet is used. \n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
